cc610780c911ce3f78b6c396f69c63055b4a5243
cotidie <daily142857@gmail.com>
2024-01-25 12:12:04 +0900

    feat: main.py 스켈레톤 완성

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from keras.datasets import mnist
-import matplotlib.pyplot as plt
-from dnn import Layer, Network
-        
+from dnn import Layer, Network, InitMethod, Softmax, Sigmoid
+import numpy as np
+
+def convert_to_one_hot(numbers: np.ndarray) -> np.ndarray:
+    converted = np.zeros((numbers.size, 10))
+    converted[np.arange(numbers.size), numbers] = 1
+    return converted
+
 if __name__ == "__main__":
-    # x_train: 손글씨 이미지(28x28), 60,000개
-    # y_train: 0~9 클래스값, 60,000개
+    # x: 손글씨 이미지(28x28)
+    # y: 0~9 클래스값
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    
-    # Deep Neural Network (3개 레이어)
+    y_train = convert_to_one_hot(y_train)
+    y_test = convert_to_one_hot(y_test)
+
     network = Network(
         layers=[
-            Layer(28*28, 128),  # 은닉층 1: 784(28*28) 입력, 128개 유닛
-            Layer(128, 64),     # 은닉층 2: 128개 입력, 64개 유닛
-            Layer(64, 10)       # 출력: 64개 입력, 10개 출력 
+            Layer(28*28, 128, activation=Sigmoid()),  # 은닉층 1: 784개 입력, 128개 출력
+            Layer(128, 64, activation=Sigmoid()),     # 은닉층 2: 128개 입력, 64개 출력
+            Layer(64, 10, activation=Softmax())       # 출력층:    64개 입력, 10개 출력 
         ]
     )
-    print(type(x_train))
-    # network.train(epoch=10, learnRate=0.001)
+    network.initialize(InitMethod.XAVIER)
+    network.train(x_train, y_train, epoch=10, learnRate=0.001)
+    network.test_accuracy(x_test, y_test)
